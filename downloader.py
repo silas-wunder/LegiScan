383636f5bca52d9f4675b2ac021f77928e4e2ef4
silas-wunder <55813205+silas-wunder@users.noreply.github.com>
2023-03-22 14:59:04 -0600

    commented out some stuff for testing

--- downloader.py
+++ downloader.py
@@ -6,15 +6,15 @@ with open("./key.txt", "r") as f:
     api_key = f.read()
 
 # TODO: check if this works
-with open("./input-caches.json", "r") as f:
-    last_update_hashes = json.loads(f.read())
+try:
+    with open("./input-caches.json", "r") as f:
+        last_update_hashes = json.loads(f.read())
+except FileNotFoundError:
+    last_update_hashes = {}
 
-# This operation will return a list of all available datasets
-list_op = "getDatasetList"
-
-# With dataset list, this will be a for loop that runs for every dataset
-
-list_url = f"https://api.legiscan.com/?key={api_key}&op={list_op}"
+# Run through the list of all available datasets and check for changes
+# If there are changes, redownload that dataset and save to input
+list_url = f"https://api.legiscan.com/?key={api_key}&op=getDatasetList"
 list_response = urlopen(list_url)
 list_json_data = json.loads(list_response.read())
 
@@ -24,16 +24,17 @@ for dataset in list_json_data["datasetlist"]:
     did = dataset["session_id"]
     if last_update_hashes[did][0] == current_hash:
         continue
-    access = dataset["access_key"]
-    dataset_url = f"https://api.legiscan.com/?key={api_key}&op=getDataset&id={did}&access_key={access}"
-    response = urlopen(dataset_url)
-    json_data = json.loads(response.read())
-    b = base64.b64decode(json_data["dataset"]["zip"])
-    z = zipfile.ZipFile(io.BytesIO(b))
-    z.extractall("./input/")
+    # NOTE: DO NOT UNCOMMENT AND RUN WITHOUT ENSURING EVERYTHING WORKS AND YOU HAVE TIME TO RUN THIS
+    # access = dataset["access_key"]
+    # dataset_url = f"https://api.legiscan.com/?key={api_key}&op=getDataset&id={did}&access_key={access}"
+    # response = urlopen(dataset_url)
+    # json_data = json.loads(response.read())
+    # b = base64.b64decode(json_data["dataset"]["zip"])
+    # z = zipfile.ZipFile(io.BytesIO(b))
+    # z.extractall("./input/")
     # TODO: check if this works
-    last_update_hashes[did] = (current_hash, json_data["dataset"]["zip"])
+    # last_update_hashes[did] = (current_hash, json_data["dataset"]["zip"])
+    last_update_hashes[did] = (current_hash, "ZIP GOES HERE")
 
-# TODO: check if this works
 with open("./input-caches.json", "w") as f:
     f.write(json.dumps(last_update_hashes))
