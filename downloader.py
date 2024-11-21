130864676ad31004f04000198917c2b3af4fd7fe
Silas Wunder <55813205+silas-wunder@users.noreply.github.com>
2024-11-20 23:37:43 -0700

    moved large files to different drive

--- downloader.py
+++ downloader.py
@@ -8,7 +8,7 @@ with open("legiscan_key.txt", "r") as f:
 
 # Grab cached info to check for changes
 try:
-    with open(r".\input-caches.json", "r") as f:
+    with open(r"E:\Big Input Data Stuff\LegiScan\input-caches.json", "r") as f:
         last_update_hashes = json.loads(f.read())
 except FileNotFoundError:
     last_update_hashes = {}
@@ -45,11 +45,11 @@ for dataset in list_json_data["datasetlist"]:
     json_data = json.loads(response.read())
     b = base64.b64decode(json_data["dataset"]["zip"])
     z = zipfile.ZipFile(io.BytesIO(b))
-    z.extractall(r"D:\Big Input Data Stuff\LegiScan\input")
+    z.extractall(r"E:\Big Input Data Stuff\LegiScan\input")
     last_update_hashes[did] = (current_hash, json_data["dataset"]["zip"])
 
 # Write out hashes for change detection
-with open("./input-caches.json", "w") as f:
+with open(r"E:\Big Input Data Stuff\LegiScan\input-caches.json", "w") as f:
     f.write(json.dumps(last_update_hashes))
 
 with open("./times.txt", "a+") as f:
